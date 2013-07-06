a2eff9316d51bde0b421b3b5e8871fc0ae894fb1
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-07-06 13:23:52 -0400

    Used a shortcut to render the index view

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
 from polls.models import Poll
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, poll_id):
